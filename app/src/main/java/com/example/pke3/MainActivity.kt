388c534903a7fc8944d47cacfd23634ac5e7b99e
bimbim1214 <bimoadityapangestu@gmail.com>
2024-10-18 20:25:31 +0700

    menambahkan fungsi composable image untuk menampilkan garmbar di dalam UI dan menambahkan clip untuk membuat sudut gambar menjadi bulat

--- app/src/main/java/com/example/pke3/MainActivity.kt
+++ app/src/main/java/com/example/pke3/MainActivity.kt
@@ -3,22 +3,27 @@ package com.example.pke3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pke3.ui.theme.Pke3Theme
 
@@ -73,6 +78,12 @@ fun BasicCompose(
             fontWeight = FontWeight.Bold,
             fontSize = 35.sp,
         )
+        Image(painter = painterResource(id = R.drawable.fifa),
+            contentDescription = null,
+            modifier  = Modifier
+                .clip(RoundedCornerShape(10.dp))
+
+        )// atau ""
     }
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
