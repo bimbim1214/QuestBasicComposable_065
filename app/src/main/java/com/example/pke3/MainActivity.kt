65b5b565bf91abe32e1208c7c6a8fe5599c1ccb1
bimbim1214 <bimoadityapangestu@gmail.com>
2024-10-18 20:47:05 +0700

    menambahkan function composable Text dengan text Bimo Aditya Pangestu

--- app/src/main/java/com/example/pke3/MainActivity.kt
+++ app/src/main/java/com/example/pke3/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -89,6 +90,9 @@ fun BasicCompose(
             fontWeight = FontWeight.W800,
             fontSize = 20.sp,
         )
+        Text(
+            text = "Bimo Aditya Pangestu",
+
     }
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
